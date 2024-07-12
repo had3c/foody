99c7bf5961b69a9fb0369e67cac1f398b8ad1bb1
EldostJs <mirzeyev0308@gamil.com>
2024-07-12 10:31:57 +0400

    login-page-fixed-bugs

--- src/pages/Login/components/LogHeader.jsx
+++ src/pages/Login/components/LogHeader.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import SegmentImg from '../../../assets/icons/segment.svg'
 import FoodyIcon from '../../../assets/icons/logoFooter.svg'
-import Flag from '../../../assets/images/langEng.svg'
 import StyleLogoHed from '../style/LoginHead.module.css'
+import LanguageBox from '../../common/components/LanguageBox/LanguageBox'
 
 function LogHeader() {
     return (
         <header className={StyleLogoHed.header}>
             <div className={StyleLogoHed.leftSection}>
-                <img className={StyleLogoHed.segmentIcon} src={SegmentImg} alt="Menu" />
                 <img className={StyleLogoHed.logoIcon} src={FoodyIcon} alt="Foody" />
             </div>
-            <div className={StyleLogoHed.rightSection}>
-                <img className={StyleLogoHed.flagIcon} src={Flag} alt="Language" />
-            </div>
+                <LanguageBox
+                className={StyleLogoHed.lang}
+                />
         </header>
     )
 }
