a2f8013753a8f7e6594fe599535a736f48d629fb
EldostJs <mirzeyev0308@gamil.com>
2024-07-23 03:55:16 +0400

    login-page-created-radio-gender-select

--- src/pages/Login/components/form/Form.jsx
+++ src/pages/Login/components/form/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { initialValues, schemas } from '../../../../utils/helper';
 import Input from '../input/Input';
 import StyleForm from '../../style/Form.module.css';
@@ -9,9 +9,7 @@ import RegisImg from '../../../../assets/images/registerImg.svg';
 import { useAuth } from '../../../../context/AuthContext'
 
 function LoginForm() {
-
     const { generateUserLoginDatas } = useAuth()
-
     const [isRegistering, setIsRegistering] = useState(false);
 
     return (
@@ -47,65 +45,79 @@ function LoginForm() {
                         validationSchema={isRegistering ? schemas.register : schemas.login}
                         onSubmit={() => console.log('Submitted')}
                     >
-                        <Form>
-                            {isRegistering ? (
-                                <>
-                                    <Input
-                                        label="Full Name"
-                                        name="fullName"
-                                        id="fullName"
-                                        placeholder="Write Your Full Name"
-                                    />
-                                    <Input
-                                        label="Username"
-                                        name="userName2"
-                                        id="userName2"
-                                        placeholder="Write Your Username"
-                                    />
-                                    <Input
-                                        label="Email"
-                                        name="email"
-                                        id="email"
-                                        placeholder="Write Your Email"
-                                    />
-                                    <Input
-                                        label="Password"
-                                        name="password"
-                                        id="password"
-                                        placeholder="Create Your Password"
-                                        type="password"
-                                    />
-                                </>
-                            ) : (
-                                <>
-                                    <Input
-                                        label="Username"
-                                        name="userName"
-                                        id="userName"
-                                        placeholder="Write Your Username"
-                                    />
-                                    <Input
-                                        label="Password"
-                                        name="password"
-                                        id="password"
-                                        placeholder="Write Your Password"
-                                        type="password"
-                                    />
-                                </>
-                            )}
-                            <button
-                                type="submit"
-                                className={StyleForm.submitButton}
-                                onClick={() => {
-                                    generateUserLoginDatas({
-                                        userName: 'qwerty',
-                                        password: 'asdfg'
-                                    })
-                                }}
-                            >
-                                {isRegistering ? 'Register' : 'Log in'}
-                            </button>
-                        </Form>
+                        {({ errors, touched }) => (
+                            <Form>
+                                {isRegistering ? (
+                                    <>
+                                        <Input
+                                            label="Full Name"
+                                            name="fullName"
+                                            id="fullName"
+                                            placeholder="Write Your Full Name"
+                                        />
+                                        <Input
+                                            label="Username"
+                                            name="userName2"
+                                            id="userName2"
+                                            placeholder="Write Your Username"
+                                        />
+                                        <Input
+                                            label="Email"
+                                            name="email"
+                                            id="email"
+                                            placeholder="Write Your Email"
+                                        />
+                                        <Input
+                                            label="Password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="Create Your Password"
+                                            type="password"
+                                        />
+                                        <div className={StyleForm.selectField}>
+                                            <label htmlFor="gender">Gender</label>
+                                            <Field as="select" name="gender" id="gender">
+                                                <option value="" disabled>Select gender</option>
+                                                <option value="male">Male</option>
+                                                <option value="female">Female</option>
+                                                <option value="other">Other</option>
+                                            </Field>
+                                            {errors.gender && touched.gender && (
+                                                <div className={StyleForm.errorMessage}>{errors.gender}</div>
+                                            )}
+                                        </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Input
+                                            label="Username"
+                                            name="userName"
+                                            id="userName"
+                                            placeholder="Write Your Username"
+                                        />
+                                        <Input
+                                            label="Password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="Write Your Password"
+                                            type="password"
+                                        />
+                                    </>
+                                )}
+                                <button
+                                    type="submit"
+                                    className={StyleForm.submitButton}
+                                    onClick={() => {
+                                        generateUserLoginDatas({
+                                            userName: 'qwerty',
+                                            password: 'asdfg'
+                                        })
+                                    }}
+                                >
+                                    {isRegistering ? 'Register' : 'Log in'}
+                                </button>
+                            </Form>
+                        )}
                     </Formik>
                 </div>
             </div>
