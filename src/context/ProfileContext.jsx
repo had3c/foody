d8147f4d9f10cc9c4a9b9c56c19353c81b5aa6f2
elnararizv <rizvanovaelnara15@gmail.com>
2024-08-09 13:31:07 +0400

    updated- profile context

--- src/context/ProfileContext.jsx
+++ src/context/ProfileContext.jsx
@@ -4,7 +4,7 @@ const ProfileContext = createContext();
 
 export function ProfileProvider({ children }) {
   const [userData, setUserData] = useState([]);
-  const [profileImage, setProfileImage] = useState(localStorage.getItem('profileImage') || 'path/to/default/image.svg');
+  const [profileImage, setProfileImage] = useState(localStorage.getItem('profileImage') || '');
   const [fullName, setFullName] = useState(localStorage.getItem('fullName') || '');
 
   const updateProfile = (image, name) => {
