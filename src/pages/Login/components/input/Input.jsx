6498b4a8c285311899f301f9eca11b4c4e57c8bc
EldostJs <mirzeyev0308@gamil.com>
2024-08-14 03:33:11 +0400

    login-page-create-input-eye

--- src/pages/Login/components/input/Input.jsx
+++ src/pages/Login/components/input/Input.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Field, ErrorMessage as Error } from 'formik';
 import StyleInput from '../../style/Input.module.css';
+import EyeInput from '../../../../assets/images/remove_red_eye.svg';
+import EyeSlashInput from '../../../../assets/images/eye_closed_icon_173166.png'
 
 function Input({ id, label, name, placeholder, type = 'text' }) {
+    const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!isPasswordVisible);
+    };
+
+    const hidePassword = () => {
+        setPasswordVisible(false);
+    };
+
+    const inputType = type === 'password' ? (isPasswordVisible ? 'text' : 'password') : type;
+
     return (
         <div className={StyleInput.cont}>
             <label htmlFor={id}>{label}</label>
-            <Field name={name} id={id} placeholder={placeholder} type={type} />
+            <div className={StyleInput.inputWrapper}>
+                <Field 
+                    name={name} 
+                    id={id} 
+                    placeholder={placeholder} 
+                    type={inputType} 
+                    className={StyleInput.inputField}
+                />
+                {type === 'password' && (
+                    <img 
+                        src={isPasswordVisible ? EyeSlashInput : EyeInput} 
+                        alt="toggle password visibility" 
+                        className={StyleInput.eyeIcon} 
+                        onClick={togglePasswordVisibility}
+                        onMouseLeave={hidePassword} // Скрываем пароль при убирании курсора
+                    />
+                )}
+            </div>
             <Error name={name}>{(error) => <span className={StyleInput.error}>{error}</span>}</Error>
         </div>
     );
