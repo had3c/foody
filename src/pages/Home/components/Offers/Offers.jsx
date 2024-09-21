1b64df83bd3c773548c1a525e30a51608e4efbb6
elnararizv <rizvanovaelnara15@gmail.com>
2024-09-21 16:26:47 +0400

    added dynamic data

--- src/pages/Home/components/Offers/Offers.jsx
+++ src/pages/Home/components/Offers/Offers.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import style from './Offers.module.css'
-import secKfc from '../../../../assets/images/secKfc.svg'
-import secPizza from '../../../../assets/images/secPizza.svg'
-import secFrench from '../../../../assets/images/secFrench.svg'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import style from './Offers.module.css';
 
 function Offers() {
-  
+  const [offers, setOffers] = useState([]);
+
+  const fetchOffers = async () => {
+    const offersUrl = `https://firestore.googleapis.com/v1/projects/foody-b6c94/databases/(default)/documents/offers`;
+
+    try {
+      const response = await axios.get(offersUrl);
+      const offersData = response.data.documents.map(doc => ({
+        id: doc.fields.id.stringValue,
+        image: doc.fields.image.stringValue,
+        title: doc.fields.title.stringValue,
+        description: doc.fields.description.stringValue,
+        createTime: doc.createTime,
+      }));
+
+      offersData.sort((a, b) => new Date(a.createTime) - new Date(b.createTime));
+      setOffers(offersData);
+    } catch (err) {
+      console.error('Error fetching offers', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchOffers();
+  }, []);
+
   return (
     <div>
-          <section className={style.offers}>
-        <div className={style.promo}>
-          <div className={style.promoTxt}>
-            <h2>Menu That Always Make You Fall In Love</h2>
-            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+         <section className={style.offers}>
+        {offers.map((offer, index) => (
+          <div key={offer.id} className={`${style.promo} ${index % 2 !== 0 ? style.rowReverse : ''}`}>
+            <div className={style.promoTxt}>
+              <h2>{offer.title}</h2>
+              <p>{offer.description}</p>
+            </div>
+            <div className={style.promoImg}>
+              <img src={offer.image} alt={offer.title} />
+            </div>
           </div>
-          <div className={style.promoImg} >
-            <img src={secKfc} alt="" />
-          </div>
-        </div>
-
-        <div className={`${style.promo} ${style.rowReverse}`}>
-          <div className={style.promoTxt}>
-            <h2>Yummy Always Papa John’s Pizza.Agree?</h2>
-            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-          </div>
-          <div className={style.promoImg} >
-            <img src={secPizza} alt="" />
-          </div>
-        </div>
-
-        <div className={style.promo}>
-          <div className={style.promoTxt}>
-            <h2>Do You Like French Fries? Mmm...</h2>
-            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-          </div>
-          <div className={style.promoImg} >
-            <img src={secFrench} alt="" />
-          </div>
-        </div>
+        ))}
       </section>
     </div>
-  )
+  );
 }
 
-export default Offers
+export default Offers;
